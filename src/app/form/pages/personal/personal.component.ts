05663765d05f0708c58017c54e9af00de2218009
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-02-10 12:06:33 -0500

    Refactor: Componente personal, finalizado

--- src/app/form/pages/personal/personal.component.ts
+++ src/app/form/pages/personal/personal.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.component.html',
-  styleUrls: ['./personal.component.scss']
+  styleUrls: ['./personal.component.scss'],
 })
 export class PersonalComponent implements OnInit {
+  myForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', Validators.required],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  fieldInvalid(field: string): boolean | null | undefined {
+    return this.myForm.get(field)?.errors && this.myForm.get(field)?.touched;
   }
 
+  saveForm(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 }
