294bbbc3710fe18e4427edb812cc4377d727bf00
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-02-10 14:37:47 -0500

    Plan finalizado

--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { FormRoutingModule } from './form-routing.module';
 
@@ -16,6 +16,6 @@ import { FinishComponent } from './pages/finish/finish.component';
     PickComponent,
     FinishComponent,
   ],
-  imports: [CommonModule, FormRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, FormRoutingModule, FormsModule, ReactiveFormsModule],
 })
 export class FormModule {}
--- src/app/form/pages/plan/plan.component.ts
+++ src/app/form/pages/plan/plan.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface CardItem {
+  image: string;
+  name: string;
+  price: number;
+}
 
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
-  styleUrls: ['./plan.component.scss']
+  styleUrls: ['./plan.component.scss'],
 })
 export class PlanComponent implements OnInit {
+  listCards: CardItem[] = [
+    {
+      image: '../../../../assets/images/icon-arcade.svg',
+      name: 'Arcade',
+      price: 9,
+    },
+    {
+      image: '../../../../assets/images/icon-advanced.svg',
+      name: 'Advanced',
+      price: 12,
+    },
+    {
+      image: '../../../../assets/images/icon-pro.svg',
+      name: 'Pro',
+      price: 15,
+    },
+  ];
 
-  constructor() { }
+  descuento: boolean = false;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  get strDescuento(): string {
+    return !this.descuento ? 'mo' : 'yr';
   }
 
+  selected(index: number): void {
+    const cards = document.getElementsByClassName('plan__item');
+
+    for (let i = 0; i < cards.length; i++) {
+      const current = cards[i];
+      if (
+        !current.classList.contains('plan__item--selected') &&
+        index === Number(current.id)
+      ) {
+        current.classList.add('plan__item--selected');
+      } else {
+        current.classList.remove('plan__item--selected');
+      }
+    }
+  }
+
+  back(): void {
+    this.router.navigate(['/multi-step/your-info']);
+  }
+
+  next(): void {
+    this.router.navigate(['/multi-step/add-ons']);
+  }
 }
